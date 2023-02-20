dfb6cc87f2e5427078d73518e5b4a778012eaf4b
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-20 10:15:33 -0600

    set up react router

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Navbar } from "./";
+import { Outlet } from 'react-router-dom';
 
 function Main() {
 
   return (
     <div id="main">
       <Navbar />
-
+      <Outlet />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Main } from "./components";
+import {
+  Route,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements
+} from 'react-router-dom';
 
 const container = document.getElementById("app");
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='' element={<Main />} />
+  )
+)
 
 const root = ReactDOM.createRoot(container);
-root.render(<Main />);
+root.render(<RouterProvider router={router} />);
