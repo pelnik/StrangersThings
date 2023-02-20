d5f2be9c11ab7bf1f6bed5102cac1dc1ae407441
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-20 13:34:38 -0600

    login form ready for API

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,28 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function LoginForm() {
+  const [typedUsername, setTypedUsername] = useState("");
+  const [typedPassword, setTypedPassword] = useState("");
 
 
+
+  function onChangeHandler(evt, setState) {
+    setState(evt.target.value);
+  }
+
+  function onClickHandler(evt) {
+    console.log(evt);
+    setTypedUsername("");
+    setTypedPassword("");
+  }
 
   return (
-    <div id="loginContainer">
-      <form id="loginForm">
-        <div id="loginUsernameContainer">
-          <label for="username">Username:</label>
-          <input type="text" id="loginUsername" name="loginUsername" />
-        </div>
-        <div id="loginPasswordContainer">        
-          <label for="password">Password:</label>
-          <input type="text" id="loginPassword" name="loginPassword" />
-        </div>
-        <div id="loginSubmitContainer">
-          <input type="submit" value="Login" />
-          <input type="submit" value="Register" />
-        </div>
-      </form>
+    <div id="loginFormContainer">
+      <div id="loginUsernameContainer">
+        <p>Username:</p>
+        <input
+          type="text"
+          id="loginUsername"
+          name="loginUsername"
+          value={typedUsername}
+          onChange={
+            (evt) => {
+              onChangeHandler(evt, setTypedUsername)
+            }
+          }
+        />
+      </div>
+      <div id="loginPasswordContainer">
+        <p>Password:</p>
+        <input
+          type="text"
+          id="loginPassword"
+          name="loginPassword"
+          value={typedPassword}
+          onChange={
+            (evt) => {
+              onChangeHandler(evt, setTypedPassword)
+            }
+          }
+        />
+      </div>
+      <div id="loginSubmitContainer">
+        <button value="Login" onClick={onClickHandler}>Login</button>
+        <button value="Register" onClick={onClickHandler}>Register</button>
+      </div>
     </div>
-  )
+  );
 }
-
 
 export default LoginForm;
