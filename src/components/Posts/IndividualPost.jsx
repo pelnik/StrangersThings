c864cf50efdfe71a61b59347eec3a4414811613a
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-22 10:32:33 -0600

    added send delete DOM elememts

--- src/components/Posts/IndividualPost.jsx
+++ src/components/Posts/IndividualPost.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { deleteSubmission } from '../../api-adapter'
 
 
 const IndividualPost = ({postData, userToken, posts, setPosts}) => {
-  const className = postData.isAuthor ? 'individual-post my-post' : 'individual-post';
+  const [messageBox, setMessageBox] = useState(false);
+
+
+  const className = postData.isAuthor ? 'individual-post-content my-post' : 'individual-post-content';
   const myPostHeader = postData.isAuthor ? <h3>Your post</h3> : null;
 
   async function onClickDelete() {
@@ -21,19 +24,40 @@ const IndividualPost = ({postData, userToken, posts, setPosts}) => {
     }
   }
 
-  const myPostDelete = postData.isAuthor
-    ? <button id="my-post-delete" onClick={onClickDelete}>Delete</button>
-    : null;
+  function onClickOpenMessage() {
+    setMessageBox(!messageBox);
+  }
+
+  function authUserPostArea(token, isAuthor) {
+    if (token === null) {
+      return null;
+    }
+
+    if (token !== null && isAuthor) {
+      return <button id="my-post-delete" onClick={onClickDelete}>Delete</button>;
+    } else if (token !== null && !isAuthor) {
+      return <button id="send-message" onClick={onClickOpenMessage}>Send Message</button>
+    }
+  }
+
+
+  const authUserPostInfo = authUserPostArea(userToken, postData.isAuthor);
+
+
+
+
 
   return (
     <div className={className}>
-      {myPostHeader}
-      <div>Post Title: {postData.title}</div>
-      <div>Post Description: {postData.description}</div>
-      <div>Post Price: {postData.price}</div>
-      <div>Post Author: {postData.author.username}</div>
-      <div>Will Deliver: {postData.willDeliver ? 'Yes' : 'No'}</div>
-      {myPostDelete}
+      <div className='individual-post'>
+        {myPostHeader}
+        <div>Post Title: {postData.title}</div>
+        <div>Post Description: {postData.description}</div>
+        <div>Post Price: {postData.price}</div>
+        <div>Post Author: {postData.author.username}</div>
+        <div>Will Deliver: {postData.willDeliver ? 'Yes' : 'No'}</div>
+      </div>
+      {authUserPostInfo}
     </div>
   )
 }
