9de97a7395cf573afb52d2e09a292e8003e38411
Matthew Pelnik <matthew.pelnik@icloud.com>
2023-02-23 16:49:56 -0600

    style post message

--- src/components/Posts/PostMessageBox.jsx
+++ src/components/Posts/PostMessageBox.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { postMessages } from '../../api-adapter'
-
+import React, { useState } from "react";
+import { postMessages } from "../../api-adapter";
 
 function PostMessageBox({ userToken, postData }) {
   const [message, setMessage] = useState("");
@@ -13,24 +12,27 @@ function PostMessageBox({ userToken, postData }) {
     evt.preventDefault();
     const response = await postMessages(userToken, postData._id, message);
 
-
     if (response.success === true) {
-      setMessage('');
+      setMessage("");
     }
   }
 
   return (
-    <div className='message-content'>
-      <form onSubmit={onSubmit}>
-        <input type="text" onChange={onChangeHandler} value={message} name="message" required="required" />
-        <input type="submit" value="send" />
+    <div className="message-content">
+      <form className="message-form" onSubmit={onSubmit}>
+        <textarea
+          type="text"
+          onChange={onChangeHandler}
+          value={message}
+          name="message"
+          required="required"
+        />
+        <div className="send-message-container">
+          <input type="submit" className="send-message-button" value="send" />
+        </div>
       </form>
     </div>
-  )
+  );
 }
-
-
-
-
 
 export default PostMessageBox;
